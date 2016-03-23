ec0fffb886fcd102e6cd44de61b24f4478bb0841
andrey-tikhonov <andrey.tikhonov.mailbox@gmail.com>
2016-03-23 17:37:20 +0300

    Added unit tests

--- cl-hash-table-utils.asd
+++ cl-hash-table-utils.asd
@@ -13,4 +13,17 @@
   :depends-on (#:alexandria)
   :serial t
   :components ((:file "package")
-               (:file "cl-hash-table-utils")))
+               (:file "cl-hash-table-utils"))
+  :in-order-to ((test-op (test-op #:cl-hash-table-utils-test))))
+
+(defsystem #:cl-hash-table-utils-test
+  :description ""
+  :version "0.0.1"
+  :author "Andrey V. Tikhonov <andrey.tikhonov.mailbox@gmail.com>"
+  :licence "WTFPL"
+  :depends-on (#:cl-hash-table-utils
+               #:prove)
+  :defsystem-depends-on (:prove-asdf)
+  :components ((:test-file "test"))
+  :perform (test-op :after (op c)
+                    (funcall (intern #.(string :run) :prove) c)))
---
+++ test.lisp
@@ -0,0 +1,106 @@
+(in-package #:cl-user)
+
+(defpackage #:cl-hash-table-utils-test
+  (:use #:cl
+        #:prove
+        #:alexandria
+        #:cl-hash-table-utils))
+
+(in-package #:cl-hash-table-utils-test)
+
+(defmacro ht (&body body)
+  `(let ((ht (make-hash-table :test #'equal)))
+     ,@body))
+
+(plan 2)
+
+(subtest "with-hash-table-items"
+
+  (is-values
+   (ht
+     (with-hash-table-items (x y z) ht
+       (setf x 1
+             y 2
+             z 3)
+       (values x
+               y
+               z)))
+   '(1 2 3))
+
+  (is-values
+   (ht
+     (with-hash-table-items ((x :x) y (z "z")) ht
+       (setf x 1
+             y 2
+             z 3))
+     (values (gethash :x  ht)
+             (gethash 'y  ht)
+             (gethash "z" ht)))
+   '(1 2 3))
+
+  (is-values
+   (ht
+     (let ((*keyfn* #'make-keyword))
+       (with-hash-table-items (x y z) ht
+         (setf x 1
+               y 2
+               z 3)))
+     (values (gethash :x ht)
+             (gethash :y ht)
+             (gethash :z ht)))
+   '(1 2 3))
+
+  (is-values
+   (ht
+     (let ((*keyfn* (compose #'string-downcase #'string)))
+       (with-hash-table-items ((foo "Foo") (bar "Bar") baz) ht
+         (setf foo 1
+               bar 2
+               baz 3)))
+     (values (gethash "Foo" ht)
+             (gethash "Bar" ht)
+             (gethash "baz" ht)))
+   '(1 2 3))
+
+  (is-values
+   (ht
+     (with-hash-table-items ((foo nil)) ht
+       (setf foo nil))
+     (gethash nil ht))
+   '(NIL T))
+
+  (is-error
+   (macroexpand
+    `(with-hash-table-items (a (b :b :x) c) ht
+       (values a b c)))
+   'type-error
+   "item-entry check type failed when macroexpand")
+  
+  (is-error
+   (ht
+     (let ((*keyfn* "foo"))
+       (with-hash-table-items (a) ht
+         a)))
+   'type-error
+   "*keyfn* check type failed"))
+
+
+(subtest "with-hash-table-values"
+
+  (is-values
+   (ht
+     (with-hash-table-items (a b c) ht
+       (setf a 1
+             b 2
+             c 3))
+     (with-hash-table-values (a b c) ht
+       (setf a :a
+             b :b
+             c :c))
+     (with-hash-table-values (a b c) ht
+       (values a
+               b
+               c)))
+   '(1 2 3)))
+
+(finalize)

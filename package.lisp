bbdb13e7f852b97300aacae95b101135e5c17ea2
andrey.tikhonov <andrey.tikhonov.mailbox@gmail.com>
2016-03-24 13:34:20 +0300

    % suffix renamed to -fn and refactoring

--- package.lisp
+++ package.lisp
@@ -2,8 +2,8 @@
 
 (defpackage #:cl-hash-table-da
   (:use #:cl)
-  (:export #:with-hash-table-values%
+  (:export #:with-hash-table-values-fn
            #:with-hash-table-values
-           #:with-hash-table-items%
+           #:with-hash-table-items-fn
            #:with-hash-table-items
            #:*keyfn*))
